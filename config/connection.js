3a421b567ebada4be9ca2808bc1473c44094148b
Albert Kim <miktrebla92@gmail.com>
2018-06-25 15:17:30 -0700

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
 var mysql = require("mysql");
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "ahndong92",
+    database: "burgers_db"
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "ahndong92",
-  database: "burgers_db"
-});
+  });
+}
+
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
